d272806f4103eb9c1b8684aabbadfd7e73fde0ff
eleadran <eleadran@gmail.com>
2025-02-20 20:11:36 -0500

    metodos de asistencia

--- src/model/persona.java
+++ src/model/persona.java
@@ -3,5 +3,16 @@ import  model.Curso;
 import org.w3c.dom.ls.LSOutput;
 
 public class persona {
+
     public static void
+
+
+    Public bolean registrar Asistencia(
+            string codIdentificador,
+            string fechaAsistencia,
+            string HoraAsistencia,
+            string ValorAsistencia,
+    ){
+        return true;
+    }
 }
